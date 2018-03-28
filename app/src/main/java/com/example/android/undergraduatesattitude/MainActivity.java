aa2912235462ee2f49ed508f72181df22882a43d
Hosam <hossmnasser938@gmail.com>
2018-03-28 16:54:24 +0200

    Allow navigation to CreateProfileLayout from MainLayout

--- app/src/main/java/com/example/android/undergraduatesattitude/MainActivity.java
+++ app/src/main/java/com/example/android/undergraduatesattitude/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.undergraduatesattitude;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void navigateToCreateProfile(View view){
+        navigateTo(this , CreateProfileActivity.class);
+    }
+
+    static void navigateTo(Context context, Class activity){
+        Intent intent = new Intent(context , activity.getClass());
+        context.startActivity(intent);
+    }
+
 }
