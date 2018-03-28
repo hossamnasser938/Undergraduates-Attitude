89344d471c328828e48238bef12a45b052e53002
Hosam <hossmnasser938@gmail.com>
2018-03-28 14:50:28 +0200

    Add Activity class

--- app/src/main/java/com/example/android/undergraduatesattitude/Activity.java
+++ app/src/main/java/com/example/android/undergraduatesattitude/Activity.java
@@ -5,4 +5,38 @@ package com.example.android.undergraduatesattitude;
  */
 
 public class Activity {
+
+    private String name;
+    private String category;
+    private ActivityDuration duration;
+
+    Activity(String name, String category, ActivityDuration duration){
+        this.name = name;
+        this.category = category;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public ActivityDuration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(ActivityDuration duration) {
+        this.duration = duration;
+    }
 }
