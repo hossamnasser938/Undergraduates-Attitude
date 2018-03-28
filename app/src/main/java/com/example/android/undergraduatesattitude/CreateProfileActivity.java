948d330c1dfd5c0e7a6c5d40a4e30bf3c67803ac
Hosam <hossmnasser938@gmail.com>
2018-03-28 17:04:23 +0200

    Allow navigation to SubMainLayout from CreateProfileLayout

--- app/src/main/java/com/example/android/undergraduatesattitude/CreateProfileActivity.java
+++ app/src/main/java/com/example/android/undergraduatesattitude/CreateProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.android.undergraduatesattitude;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class CreateProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+    }
+
+    private void navigateToSubMainLayout(View view){
+        MainActivity.navigateTo(this , SubMainActivity.class);
     }
 }
