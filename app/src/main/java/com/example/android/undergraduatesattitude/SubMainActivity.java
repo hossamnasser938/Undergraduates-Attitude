78339481f89f7e5414e95d442ad07c245ed64046
Hosam <hossmnasser938@gmail.com>
2018-04-08 18:08:55 +0200

    Add SubMainLayout

--- app/src/main/java/com/example/android/undergraduatesattitude/SubMainActivity.java
+++ app/src/main/java/com/example/android/undergraduatesattitude/SubMainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.undergraduatesattitude;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SubMainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class SubMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub_main);
+    }
+
+    public void navigateToAddCourseActivity(View view){
+        Intent intent = new Intent(getApplicationContext(), AddCourseActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToAddActivityActivity(View view){
+        Intent intent = new Intent(getApplicationContext(), AddActivityActivity.class);
+        startActivity(intent);
+    }
+
+    public void navigateToReportActivity(View view){
+        Intent intent = new Intent(getApplicationContext(), ReportActivity.class);
+        startActivity(intent);
     }
 }
