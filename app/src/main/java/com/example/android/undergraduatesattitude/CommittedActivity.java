3b004cc94a93d27580038b763f5d70b39eb9d20f
abdelkhalik <abdelkhalik.gamal@gmail.com>
2018-04-10 04:06:54 +0200

    update of inference engine

--- app/src/main/java/com/example/android/undergraduatesattitude/CommittedActivity.java
+++ app/src/main/java/com/example/android/undergraduatesattitude/CommittedActivity.java
@@ -1,76 +1,111 @@
 package com.example.android.undergraduatesattitude;
 
-
 /**
  *
  * @author abdelkhalik
  */
 public class CommittedActivity extends OptimalActivity {
 
-    ActivityDuration committedDuration;
     private String advice;
+    
 
-    public CommittedActivity(String name, Category category, ActivityDuration max, ActivityDuration min, Priority priority, String advice, ActivityDuration committedDuration) {
-        super(name, category, max, min, priority);
-        this.committedDuration = committedDuration;
-
+    public CommittedActivity(Category category, String name, ActivityDuration committedDuration) {
+        super(category, name, KnowledgeBase.getActivityOptimalMaxDuration(name), KnowledgeBase.getActivityOptimalMinDuration(name), KnowledgeBase.getActivityPriority(name));
+        this.setDuration(committedDuration);
     }
 
-    ;
-    
+    public ActivityDuration getCommittedDuration() {
+        return getDuration();
+    }
+
+    public void setCommittedDuration(ActivityDuration duration) {
+        this.setDuration(duration);
+    }
+
     /**
      * @return the advice
      */
     public String getAdvice() {
+        generateAdice();
         return advice;
     }
 
-    /**
-     * @param advice the advice to set
-     */
-    public void setAdvice(String advice) {
-        this.advice = advice;
-    }
-
-    public ActivityDuration setCommittedDuration() {
-        return committedDuration;
-    }
-
-    public void setCommittedDuration(ActivityDuration duration) {
-        this.committedDuration = duration;
-    }
-
-    public String generateAdice(ActivityDuration committedDuration, Priority priority, ActivityDuration max, ActivityDuration min) {
+    private void generateAdice() {
         String Advice = null;
-        double CommittedDuration = committedDuration.getHours() + ((committedDuration.getMinutes()) / 60);
-        double maxDuration = max.getHours() + ((max.getMinutes()) / 60);
-        double minDuration = min.getHours() + ((min.getMinutes()) / 60);
-        switch (priority) {
+        String optionalAdvice = "You Have more committed Duration in this Activity which the number of hours is : ";
+        String recommendedDecreaseAdvice ="You must decrease the committed duration with  ";
+        String recommendedIncreaseAdvice ="You must increase the committed duration with ";
+        String highlyRecommendedDecreaseAdvice =" may be need to decrease the committed duration with ";
+        String highlyRecommendedIncreaseAdvice ="You need to increase the committed duration with ";
+        String mandatoryDecreaseAdvice = "To much time , you must decrease the committed duration with ";
+        String mandatoryIncreaseAdvice = "Committed duration must be increase with ";
+        String forgetRecommendAdvice=" you have do this activity";
+        String forgetMandatoryAdvice=" you must do this activity";
+        String totalNumberHourseAdvice = null;
+        Report report = User.user.getWeek().getReport();
+        boolean forgetRecommendActivity = true;
+        boolean forgetMandatoryActivity = true;
+        double CommittedDuration = this.getCommittedDuration().getHours() + ((this.getCommittedDuration().getMinutes()) / 60);
+        double maxDuration = this.getMaxDuration().getHours() + ((this.getMaxDuration().getMinutes()) / 60);
+        double minDuration = this.getMinDuration().getHours() + ((this.getMinDuration().getMinutes()) / 60);
+        int allCommittedHourse = report.getAllCommitedHours();
+
+        switch (this.getPriority()) {
             case OPTIONAL:
                 if (CommittedDuration > maxDuration) {
-                    Advice = "You Have more committed Duration in this Activity which the number of hours is : "+(CommittedDuration-maxDuration)+" Hrs";
+                    Advice = optionalAdvice +(CommittedDuration - maxDuration)+ " Hrs";
 
                 }
 
                 break;
             case RECOMMENDED:
+                forgetRecommendActivity = false;
                 if (CommittedDuration > maxDuration) {
-                    Advice = "You must decrease the committed duration with  "+(maxDuration-CommittedDuration)+"Hrs";
+                    Advice = recommendedDecreaseAdvice + (CommittedDuration - maxDuration) + "Hrs";
                 } else if (CommittedDuration < minDuration) {
-                    Advice = "You must increase the committed duration with "+(CommittedDuration-minDuration)+"Hrs";
+                    Advice = recommendedIncreaseAdvice + ( minDuration - CommittedDuration) + "Hrs";
+                }
+                break;
+            case HIGHLY_RECOMMENDED:
+                forgetRecommendActivity = false;
+                if (CommittedDuration > maxDuration) {
+                    Advice = highlyRecommendedDecreaseAdvice + ( CommittedDuration - maxDuration ) + "Hrs";
+                } else if (CommittedDuration < minDuration) {
+                    Advice = highlyRecommendedIncreaseAdvice + ( minDuration - CommittedDuration ) + "Hrs";
                 }
                 break;
             case MANDATORY:
+                forgetMandatoryActivity = false;
                 if (CommittedDuration > maxDuration) {
-                    Advice = "To much time , you must decrease the committed duration with "+(maxDuration-CommittedDuration)+"Hrs to be come better !";
+                    Advice = mandatoryDecreaseAdvice + (CommittedDuration-maxDuration ) + "Hrs to be come better !";
                 } else if (CommittedDuration < minDuration) {
-                    Advice = "Committed duration must be increase with "+(CommittedDuration-minDuration)+"Hrs to reach better life";
+                    Advice = mandatoryIncreaseAdvice + (minDuration-CommittedDuration ) + "Hrs to reach better life";
                 }
                 break;
             default:
                 break;
 
         }
-        return Advice;
+        if (allCommittedHourse > report.WEEK_HOURS ) {
+            totalNumberHourseAdvice = "You enter large number of Hours is " + (allCommittedHourse) + "Hrs V.S the number of the week hours is 168 Hrs ";
+            report.setTotalNumberHoursAdvice(totalNumberHourseAdvice);
+        } else if (report.WEEK_HOURS > allCommittedHourse) {
+            totalNumberHourseAdvice = "you have " + (168 - allCommittedHourse) + "Hrs which you have to make use of this hours to do anther activity ";
+            report.setTotalNumberHoursAdvice(totalNumberHourseAdvice);
+        }
+        if (forgetMandatoryActivity == true) {
+           Advice = forgetMandatoryAdvice;
+        }
+        if (forgetRecommendActivity == true)
+        {
+            Advice =forgetRecommendAdvice;
+        }
+
+        this.advice = Advice;
     }
+
+   
+
+    
+
 }
